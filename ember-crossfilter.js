1996e3f677f66b9c60e087858fe4f5c906d9d4e5
Andrew Reedy <andrew.reedy@thecontrolgroup.com>
2014-02-14 20:43:49 -0800

    update

--- ember-crossfilter.js
+++ ember-crossfilter.js
@@ -94,16 +94,6 @@ EmberCrossfilter = Ember.Mixin.create({
    */
   _createDimensions: function() {
 
-    var content = Ember.get(this, 'content');
-
-      // Parse Dates
-    content.forEach(function(d, i) {
-      d.date = moment(d.created).toDate();
-      delete d.created;
-    });
-
-    // Date Dimension
-    this.set('dimension.date', ndx.dimension(function(d) { return d.date; }));
   },
 
 
@@ -114,25 +104,7 @@ EmberCrossfilter = Ember.Mixin.create({
    * @private
    */
   _createGroups: function() {
-    this.set('group.dateComposite', this.get('dimension.date').group(d3.time.day).reduce(
-      function(p, v){
-          return {
-            lead_count: p.lead_count+1,
-            spend: p.spend + v.price,
-            sold_count: (v.status == 'sold') ? p.sold_count + 1 : p.sold_count,
-            contacted_count: (v.status == 'contacted') ? p.contacted_count + 1 : p.contacted_count,
-          }
-      },
-      function(p, v){
-        return {
-          lead_count: p.lead_count-1,
-          spend: p.spend - v.price,
-          sold_count: (v.status == 'sold') ? p.sold_count - 1 : p.sold_count,
-          contacted_count: (v.status == 'contacted') ? p.contacted_count - 1 : p.contacted_count,
-        }
-      },
-      function(){return {lead_count:0, spend:0, sold_count:0, contacted_count: 0};}
-    ));
+
   }
 
 
